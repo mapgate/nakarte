e24bb7f399302f94a6d0b8092f2f2280f9d3f911
Nikolay Korotkiy <sikmir@gmail.com>
2020-01-13 03:05:00 +0300

    layers: add my tracks (raster)

--- src/layers.js
+++ src/layers.js
@@ -12,6 +12,7 @@ import urlViaCorsProxy from '~/lib/CORSProxy';
 import '~/lib/leaflet.layer.TileLayer.cutline';
 import {getCutline} from '~/lib/layers-cutlines';
 import {LayerCutlineOverview} from '~/lib/leaflet.layer.LayerCutlineOverview';
+import '~/lib/leaflet.layer.mytracks';
 
 class LayerGroupWithOptions extends L.LayerGroup {
     constructor(layers, options) {
@@ -1193,6 +1194,21 @@ class LayerGroupWithOptions extends L.LayerGroup {
                         }
                     )
                 },
+                {
+                    title: 'Tracks by @sikmir (raster)',
+                    isDefault: true,
+                    layer: new L.Layer.MyTracksRaster(
+                        {
+                            code: 'Mytr',
+                            isOverlay: true,
+                            isOverlayTransparent: true,
+                            scaleDependent: true,
+                            print: true,
+                            jnx: false,
+                            shortName: 'my_rtracks'
+                        }
+                    )
+                },
     ];
 
     const groupsDefs = [
@@ -1276,6 +1292,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
                 'Strava heatmap (ride)',
                 'Strava heatmap (winter)',
                 'Extremum tracks',
+                'Tracks by @sikmir (raster)',
             ],
 
         },
@@ -1362,6 +1379,7 @@ class LayerGroupWithOptions extends L.LayerGroup {
         'Wikimapia',
         'OpenRailwayMap',
         'Extremum tracks',
+        'Tracks by @sikmir (raster)',
 
         // point overlays
         'Mountain passes (Westra)',
